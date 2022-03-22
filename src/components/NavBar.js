d8823d07f35204b41442bd088cec600c907d17d9
Taylor Huffman <taylor@crucialartists.com>
2022-03-22 12:55:45 -0500

    Complete assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,22 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div className="navbar">
+    <li><NavLink to="/" exact>
+      Home
+    </NavLink></li>
+    <li><NavLink to="/movies" exact>
+      Movies
+    </NavLink></li>
+    <li><NavLink to="/directors" exact>
+      Directors
+    </NavLink></li>
+    <li><NavLink to="/actors" exact>
+      Actors
+    </NavLink></li>
+  </div>
+  )
 }
 
 export default NavBar;
